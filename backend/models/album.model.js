4f5247b03681cf65e2ebc77d86501717045069a9
parham-devdocs <p.77.pazargadi@gmail.com>
2024-12-13 23:43:35 +0330

    album  model created

---
+++ backend/models/album.model.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const albumSchema =new mongoose.Schema({
+    title: { type: String, required: true },
+    artist: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    releaseYear: { type: Number, required: true }, // Fixed typo from 'duraion' to 'duration'
+    songs: [{ type: mongoose.Schema.Types.ObjectId,ref:"Song" }]
+}, { timestamps: true }); // Add timestamps option here
+
+
+export const Song=mongoose.model("Album",albumSchema)
